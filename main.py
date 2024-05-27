5692d68b0c84c04ca6059022f2da5922b758b02a
Que5 <phumlanibrightdlamini@gmail.com>
2024-05-27 20:06:20 +0200

    removed some files and fixed the code

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
+
 
 #You can duplicate this function if you want to add more pages, you would cahnge the route and template
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station, "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
 
